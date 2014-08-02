e5c9bc336dfd47033bec764f9315de45c052ce4b
Aaron Bloomfield <aaron@virginia.edu>
2014-08-02 02:32:51 -0400

    Created the initial version of the script, which can create teams and manage repositories

---
+++ main.php
@@ -0,0 +1,357 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+$createrepos = 0;
+$manageteams = 0;
+
+$userorg = false;
+$token = false;
+$csvfile = false;
+$public = false;
+
+$reposcol = -1;
+$descscol = -1;
+$userscol = -1;
+$teamscol = -1;
+
+checkParams();
+
+// create client and authenticate
+$client = new \Github\Client(
+    new \Github\HttpClient\CachedHttpClient(array('cache_dir' => '/tmp/github-api-cache'))
+);
+$client->authenticate($token,Github\Client::AUTH_HTTP_TOKEN);
+
+// take the appropriate action
+if ( $createrepos )
+  createRepos($client);
+if ( $manageteams )
+  manageTeams($client);
+
+
+function checkParams() {
+  global $userorg, $token, $csvfile, $reposcol, $descscol, $argv, $argc, $createrepos, $manageteams, $public, $userscol, $teamscol;
+  for ( $i = 1; $i < $argc; $i++ ) {
+    switch($argv[$i]) {
+    case "-org":
+      $userorg = $argv[++$i];
+      break;
+    case "-token":
+      $token = $argv[++$i];
+    break;
+    case "-create-repos":
+      $createrepos = 1;
+      break;
+    case "-manage-teams":
+      $manageteams = 1;
+      break;
+    case "-reposcol":
+      $reposcol = $argv[++$i];
+      if ( !is_numeric($reposcol) )
+	die ("The -reposcol value must be numeric\n");
+      $reposcol--;
+      break;
+    case "-descscol":
+      $descscol = $argv[++$i];
+      if ( !is_numeric($descscol) )
+	die ("The -descscol value must be numeric\n");
+      $descscol--;
+      break;
+    case "-teamscol":
+      $teamscol = $argv[++$i];
+      if ( !is_numeric($teamscol) )
+	die ("The -teamscol value must be numeric\n");
+      $teamscol--;
+      break;
+    case "-userscol":
+      $userscol = $argv[++$i];
+      if ( !is_numeric($userscol) )
+	die ("The -userscol value must be numeric\n");
+      $userscol--;
+      break;
+    case "-csvfile":
+    case "-csv":
+      $csvfile = $argv[++$i];
+      if ( !file_exists($csvfile) )
+	die ("CSV file '$csvfile' does not exist\n");
+      break;
+    case "-public":
+      $public = true;
+      break;
+    case "-private":
+      $public = false;
+      break;
+    default:
+      die ("Unknown parameter: " . $argv[$i] . "\n");
+      break;
+    }
+  }
+
+  if ( !$createrepos && !$manageteams )
+    die ("Nothing to do!  You must specify either -create-repos or -manage-teams\n");
+  if ( $createrepos && $manageteams )
+    die ("You can only specify one mode (-create-repos or -manage-teams), not both\n");
+  if ( !$userorg )
+    die ("You must specify the github user or organization via -userorg\n");
+  if ( !$csvfile )
+    die ("You must specify a CSV file to use via -csv\n");
+  if ( !$token ) {
+    echo "Enter the API token: ";
+    $token = readline();
+  }
+}
+
+function readCSVHeader($fp) {
+  global $reposcol, $descscol, $userscol, $teamscol, $createrepos, $manageteams;
+
+  $row = fgetcsv($fp);
+  if ( $reposcol != -1 )
+    echo "Was supplied the repositories column of " . ($reposcol+1) . "\n";
+  if ( $descscol != -1 )
+    echo "Was supplied the descriptions column of " . ($descscol+1) . "\n";
+  if ( $teamscol != -1 )
+    echo "Was supplied the teams column of " . ($teamscol+1) . "\n";
+  if ( $userscol != -1 )
+    echo "Was supplied the users column of " . ($userscol+1) . "\n";
+  for ( $i = 0; $i < count($row); $i++ ) {
+    if ( ($reposcol == -1) &&
+	 ((strtolower($row[$i]) == "repo") || (strtolower($row[$i]) == "repos") ||
+	  (strtolower($row[$i]) == "repository") || (strtolower($row[$i]) == "repositories")) )
+      $reposcol = $i;
+    if ( ($descscol == -1) &&
+	 ((strtolower($row[$i]) == "desc") || (strtolower($row[$i]) == "description") ||
+	  (strtolower($row[$i]) == "descs") || (strtolower($row[$i]) == "descriptions")) )
+      $descscol = $i;
+    if ( ($userscol == -1) &&
+	 ((strtolower($row[$i]) == "user") || (strtolower($row[$i]) == "users") ||
+	  (strtolower($row[$i]) == "username") || (strtolower($row[$i]) == "usernames")) )
+      $userscol = $i;
+    if ( ($teamscol == -1) &&
+	 ((strtolower($row[$i]) == "team") || (strtolower($row[$i]) == "teams")) )
+      $teamscol = $i;
+  }
+  if ( $reposcol == -1 )
+    die ("No repos column found (and none specified via -reposcol)\n");
+  else
+    echo "Using column " . ($reposcol+1) . " with header '" . $row[$reposcol] . "' as the repository list\n";
+  if ( $createrepos )
+    if ( $descscol == -1 )
+      echo "No description column found (and none specified via -descscol); continuing without descriptions...\n";
+    else
+      echo "Using column " . ($descscol+1) . " with header '" . $row[$descscol] . "' as the descriptions list\n";
+  if ( $manageteams ) {
+    if ( $userscol == -1 )
+      die ("No users column found (and none specified via -userscol)\n");
+    else
+      echo "Using column " . ($userscol+1) . " with header '" . $row[$userscol] . "' as the users list\n";
+    if ( $teamscol == -1 )
+      die ("No teams column found (and none specified via -teamscol)\n");
+    else
+      echo "Using column " . ($teamscol+1) . " with header '" . $row[$teamscol] . "' as the teams list\n";
+  }
+}
+
+function createRepos($client) {
+  global $reposcol, $descscol, $csvfile, $userorg, $public;
+
+  try {
+
+    // read in the CSV file header, and determine the row with the repository names
+    $fp = fopen($csvfile,"r");
+    readCSVHeader($fp);
+
+    // read in the repo data
+    $newrepos = array();
+    $descriptions = array();
+    while ( ($row = fgetcsv($fp)) ) {
+      if ( !in_array($row[$reposcol],$newrepos) ) {
+	$newrepos[] = $row[$reposcol];
+	if ( $descscol != -1 )
+	  $descriptions[$row[$reposcol]] = $row[$descscol];
+	else
+	  $descriptions[$row[$reposcol]] = "no description";
+      }
+    }
+    echo "Repositories that were requested to create (" . count($newrepos) . "): " . implode(", ",$newrepos) . "\n";
+
+    // get the repos in the user/org
+    echo "Attempting to read repositories for '$userorg'...\n";
+    $tmp = $client->api('organization')->repositories($userorg);
+    $ghrepos = array();
+    foreach ( $tmp as $repo )
+      $ghrepos[] = $repo['name'];
+    echo "Repositories on github.com/$userorg (" . count($ghrepos) . "): " . implode(", ",$ghrepos) . "\n";
+
+    // which ones to create?
+    $repos_to_create = array();
+    foreach ( $newrepos as $repo )
+      if ( in_array($repo,$ghrepos) )
+	echo "\trepo '$repo' already exists on github; ignoring\n";
+      else
+	$repos_to_create[] = $repo;
+    echo "Processed repositories to create (" . count($repos_to_create) . "):\n";
+    foreach ( $repos_to_create as $repo )
+      echo "\t$repo (\"" . $descriptions[$repo] . "\")\n";
+
+    // are there any left?
+    if ( count($repos_to_create) == 0 )
+      die ("There are no repos to create!\n");
+
+    // get user prompt
+    echo "Are you sure you want to continue? (yes/no)\n";
+    $response = readline();
+    if ( $response != "yes" )
+      die ("Response of 'yes' not received.\n");
+
+    // create the repo!
+    foreach ( $repos_to_create as $repo ) {
+      $client->api('repo')->create($repo, $descriptions[$repo], "",$public,$userorg);
+      echo "Repository '$repo' created!\n";
+    }
+
+    fclose($fp);
+
+  } catch (Exception $e) {
+    die ("Exception thrown: " . $e->getMessage() . "\n");
+  }
+}
+
+function manageTeams($client) {
+  global $reposcol, $teamscol, $userscol, $csvfile, $userorg;
+
+  try {
+
+    // read in the CSV file header, and determine the row with the repository names
+    $fp = fopen($csvfile,"r");
+    readCSVHeader($fp);
+
+    // read in the team data from CSV
+    $teamdata = array();
+    $teams = array();
+    while ( ($row = fgetcsv($fp)) ) {
+      $teamdata[] = array($row[$reposcol], $row[$userscol], $row[$teamscol]);
+      if ( !in_array($row[$teamscol],$teams) )
+	$teams[] = $row[$teamscol];
+    }
+
+    echo "\nHandling of team creation...\n";
+
+    // get team data from github
+    echo "Attempting to read teams for '$userorg'...\n";
+    $tmp = $client->api('organization')->teams()->all($userorg);
+    //print_r($tmp);
+    $ghteams = array();
+    foreach ( $tmp as $team )
+      $ghteams[$team['name']] = $team['id'];
+    echo "Teams on github.com/$userorg (" . count($ghteams) . "): " . implode(", ",array_keys($ghteams)) . "\n";
+
+    // which teams to create?
+    $teams_to_create = array();
+    foreach ( $teams as $team )
+      if ( !in_array($team,array_keys($ghteams)) )
+	$teams_to_create[] = $team;
+    echo "Teams to create on github.com/$userorg (" . count($teams_to_create) . "): " . implode(", ",$teams_to_create) . "\n";
+
+    // create those teams
+    foreach ( $teams_to_create as $team ) {
+      $client->api('organization')->teams()->create($userorg,
+	           array('name'=>$team,'permission'=>'push'));
+      echo "\tteam '$team' on $userorg created!\n";
+    }
+
+    // get team data (again) from github
+    echo "Attempting to read teams (again) for '$userorg'...\n";
+    $tmp = $client->api('organization')->teams()->all($userorg);
+    //print_r($tmp);
+    $ghteams = array();
+    foreach ( $tmp as $team )
+      $ghteams[$team['name']] = $team['id'];
+    echo "Teams on github.com/$userorg (" . count($ghteams) . "): " . implode(", ",array_keys($ghteams)) . "\n";
+    //echo "Teams IDs on github.com/$userorg (" . count($ghteams) . "): " . implode(", ",$ghteams) . "\n";
+
+    // for each team, set the members
+    echo "\nHandling of member assignments to teams...\n";
+    foreach ( $teams as $team ) {
+      echo "Setting members for team '$team'...\n";
+
+      // get desired members from CSV file data
+      $desired_members = array();
+      foreach ( $teamdata as $datum ) {
+	if ( $datum[2] != $team )
+	  continue;
+	if ( !in_array($datum[1],$desired_members) )
+	  $desired_members[] = $datum[1];
+      }
+      echo "\tdesired members in team '$team' (" . count($desired_members) . "): " . implode(", ",$desired_members) . "\n";
+
+      // get actual members from github
+      $tmp = $client->api('organization')->teams()->members($ghteams[$team]);
+      $actual_members = array();
+      //print_r($tmp);
+      foreach ( $tmp as $user )
+	$actual_members[$user['login']] = $user['id'];
+      echo "\tactual members in team '$team' (" . count($actual_members) . "): " . implode(", ",array_keys($actual_members)) . "\n";
+      //echo "\tactual members IDs in team '$team' (" . count($actual_members) . "): " . implode(", ",$actual_members) . "\n";
+
+      // add members
+      foreach ( $desired_members as $member )
+	if ( !in_array($member,array_keys($actual_members)) ) {
+	  $client->api('organization')->teams()->addMember($ghteams[$team],$member);
+	  echo "\tadded $member to team '$team'\n";
+	}
+
+      // remove members
+      foreach ( array_keys($actual_members) as $member )
+	if ( !in_array($member,$desired_members) ) {
+	  $client->api('organization')->teams()->removeMember($ghteams[$team],$member);
+	  echo "\tremoved $member from team '$team'\n";
+	}
+    }
+
+    // for each team, set the repos
+    echo "\nHandling of repo assignments to teams...\n";
+    foreach ( $teams as $team ) {
+      echo "Setting repos for team '$team'...\n";
+
+      // get desired repos from CSV file data
+      $desired_repos = array();
+      foreach ( $teamdata as $datum ) {
+	if ( $datum[2] != $team )
+	  continue;
+	if ( (trim($datum[0]) != "") && (!in_array($datum[0],$desired_repos)) )
+	  $desired_repos[] = $datum[0];
+      }
+      echo "\tdesired repos for team '$team' (" . count($desired_repos) . "): " . implode(", ",$desired_repos) . "\n";
+
+      // get actual repos from github
+      $tmp = $client->api('organization')->teams()->repositories($ghteams[$team]);
+      //print_r($tmp);
+      $actual_repos = array();
+      foreach ( $tmp as $repo )
+	$actual_repos[$repo['name']] = $repo['id'];
+      echo "\tactual repos in team '$team' (" . count($actual_repos) . "): " . implode(", ",array_keys($actual_repos)) . "\n";
+
+      // add repos
+      foreach ( $desired_repos as $repo )
+	if ( !in_array($repo,array_keys($actual_repos)) ) {
+	  $client->api('organization')->teams()->addRepository($ghteams[$team],$userorg,$repo);
+	  echo "\tadded $repo to team '$team'\n";
+	}
+
+      // remove repos
+      foreach ( array_keys($actual_repos) as $repo )
+	if ( !in_array($repo,$desired_repos) ) {
+	  $client->api('organization')->teams()->removeRepository($ghteams[$team],$userorg,$repo);
+	  echo "\tremoved $repo from team '$team'\n";
+	}
+    }
+
+    fclose($fp);
+
+  } catch (Exception $e) {
+    die ("Exception thrown: " . $e->getMessage() . "\n");
+  }
+}
+
+?>
---
+++ readme.md
@@ -0,0 +1,134 @@
+Github API Tools
+================
+
+This repository is a set of tools aimed at letting course instructors configure their github organization accounts based on information in CSV files.  It allows the creation of repositories and management of teams.  This is based on, and this repo is forked from, the [KnpLabs/php-github-api](https://github.com/KnpLabs/php-github-api) repository.  This script will never delete repositories or teams.  However, it intentionally **MAY** remove users from teams.
+
+
+Installation
+------------
+
+First, the github-api-tools must be configured.  The quick version is below; the full description can be found in the [original readme](readme-original.md) ([original version from the upstream repo](https://github.com/KnpLabs/php-github-api/blob/master/README.markdown)).
+
+The first step to use `php-github-api` is to download composer:
+
+```bash
+$ curl -s http://getcomposer.org/installer | php
+```
+
+Then we have to install our dependencies using:
+```bash
+$ php composer.phar install
+```
+
+Next, the user will need to create a personal access token from [https://github.com/settings/applications](https://github.com/settings/applications).  When creating that token, it ***MUST*** be given the `admin:org` scope, as the purpose of this repo's main script is to allow the modifications of the teams and repositories in an organization.  The API token will need to be remembered, as it is entered each time the program is run.
+
+This script requires being able to execute a PHP script from the command line.  Under recent Ubuntu installations, this is provided via installing the `php5-cli` package.
+
+At that point, PHP can be run from the command-line via:
+
+```bash
+$ php main.php <options>
+```
+
+Usage
+-----
+
+There are two exclusive modes: create repositories and manage teams.  All the other options provide parameters to one of these two modes.
+
+**Modes**
+
+* `-create-repos`: Create the repositories specified in the provided CSV file.
+* `-manage-teams`: Manage teams as per the specifications in the provided CSV file.
+
+**General options**
+
+* `-org <name>`: specify the organization under which the repositories will be created or where the teams will be managed.  The user must have administration access to that organization (and the token must have that scope, as described above).
+* `-token <token>`: specify the Github personal access token to be used for authentication; see above for how to create it.  If this is not specified, the script will prompt for the token.
+* `-public`: whether to make the created repositories public; this is *not* the default.
+* `-private`: whether to make the created repositories private.  This requires that the organization have that many private repositories available.  This is the default.
+
+**CSV file options**
+
+* `-csv <file>`: the name of the CSV file that provides the information for the script to run.  See below for the format.  Note that `-csvfile <file>` is another name for this option.
+* `-reposcol <num>`: the number of the column (indexed from 1) that provides the name of the repositories to create; if not specfied, the script will search for columns with the names: 'repos', 'repo', 'repository', and 'repositories'.
+* `-descscol <num>`: the number of the column (indexed from 1) that provides the descriptions of the repositories to create; if not specfied, the script will search for columns with the names: 'desc', 'descs', 'description', 'descriptions'.
+* `-userscol <num>`: the number of the column (indexed from 1) that provides the github userids for managing the teams; if not specfied, the script will search for columns with the names: 'user', 'users', 'username', 'usernames'.
+* `-teamscol <num>`: the number of the column (indexed from 1) that provides the names of the teams to manage; if not specfied, the script will search for columns with the names: 'team', 'teams'.
+
+CSV file format
+---------------
+
+The CSV file must have the top row be the column header titles.  Which column to find the information in can either be provided via the command line options (the "CSV file options", above), or by naming the columns per the convention also mentioned above.  CSV parsing is provided via the `fgetcsv()` function in PHP, so it should be general enough to handle most CSV formats.
+
+For all of the columns, the user can specify which column holds the data (via `-reposcol`, `-desccol`, etc.).  If not provided, the script will attempt to determine which column based on the column header name; if there are multiple columns, then the first encountered (the left-most) will be used.
+
+The create repository mode requires only one column (the repository name), and can optionally have a description column.  If no description column is provided, then "no description" is provided as the description for each repo.  Additional rows with the same repository name will be ignored.
+
+The manage teams mode requires three columns: repository, github username, team name.  Additional rows will have no effect.
+
+
+Example
+-------
+
+Consider the following data (which is not a CSV file, obviously):
+
+
+id | user  | repo  | desc        | group
+---|-------|-------|-------------|-------
+1  | user1 | repo1 | Lorem ipsum | group1
+2  | user2 | repo1 | Lorem ipsum | group1
+3  | user2 | repo2 | Lorem ipsum | group2
+4  | user3 | repo2 | Lorem ipsum | group2
+5  | user4 | repo3 | Lorem ipsum | group3
+6  | user5 | repo3 | Lorem ipsum | group3
+7  | user6 | repo1 | Lorem ipsum | group2
+8  | user7 | repo2 | Lorem ipsum | group2
+9  | user8 | repo3 | Lorem ipsum | group3
+
+The actual CSV file would be:
+
+```
+id,user,repo,desc,group
+1,user1,repo1,"Lorem ipsum",group1
+2,user2,repo1,"Lorem ipsum",group1
+3,user2,repo2,"Lorem ipsum",group2
+4,user3,repo2,"Lorem ipsum",group2
+5,user4,repo3,"Lorem ipsum",group3
+6,user5,repo3,"Lorem ipsum",group3
+7,user6,repo1,"Lorem ipsum",group2
+8,user6,repo2,"Lorem ipsum",group2
+9,user6,repo3,"Lorem ipsum",group3
+```
+
+Note that the last column header is 'group', not 'team'; the former is not recognized automatically, while the latter is.
+
+First, one would run this to create the repositories:
+
+```bash
+$ php main.php -create-repos -org superorg -token 1234567890 -csv data.csv
+```
+
+The script will find the 'repo' column, and create all the repos listed there; there are three such repos to be created (repo1, repo2, and repo3).  If they already exist, then it does nothing (but it does indicate that they already exist).  Note that this assumes that the organization name is 'superorg', and that the token provided has already been created properly (see above for details).
+
+Next, one could run the script in the following manner to manage the groups:
+
+```bash
+$ php main.php -manage-teams -org superorg -teamscol 5 -csv data.csv
+```
+
+This execution does not specify a token on the command line, so the script will prompt for it.  And because the 'group' column header is not known to the script, it needs to be specified via the `-teamscol` option.
+
+This second execution will perform three different funtions:
+
+* If the teams do not exist, then they will be created.  There are three teams to be created (group1, group2, and group3).  The teams have push access by default (Github defaults to pull).
+* Looking at just the user and team (group) columns, it ensures that the **ONLY** members of the team are the ones listed.  In the data from above, group1 will contain only user1 and user2; group 2 will contain user2, user3, user6, and user7; group3 will contain user4, user5, and user8.
+* Looking just at the repository and team (group) columns, it will ensure that the appropriate teams can access those repositories, **AND NO OTHER REPOS**.  This means that it will **REMOVE** access to a repo if a team can access a repo not listed.  The intent here was probably to have group1 able to access repo1, group2 access repo2, and group3 access repo3.  However, a likely typo in line ID 7 gives group2 access to repo1.  This means *anybody* in group2 can access both repo2 (from lines 3, 4, and 8), as well as repo1 (from line 7).
+
+From the data provided above, once modified to have real values for the token, organization, and usernames, it worked as desired.
+
+Notes
+-----
+
+* If the repositories are not created when setting up the teams, the script will crash.
+* If you just want to create the teams, but not set up the repos, you can leave the repo name blank (but the repo column must still be present); however, this will REMOVE any repos that the team currently has access to
+
