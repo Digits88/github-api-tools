df224f720bc8986a2794003efbb84226527ce52e
Aaron Bloomfield <aaron@virginia.edu>
2014-10-21 12:46:32 -0400

    Can now download a list of issues

--- main.php
+++ main.php
@@ -6,6 +6,7 @@ const MODE_NONE = 0;
 const MODE_CREATE = 1;
 const MODE_TEAMS = 2;
 const MODE_UNWATCH = 3;
+const MODE_ISSUES = 4;
 
 $mode = MODE_NONE;
 
@@ -15,6 +16,9 @@ $csvfile = false;
 $public = false;
 $watch = true;
 $dryrun = false;
+$repo = false;
+$output = false;
+$quiet = false;
 
 $reposcol = -1;
 $descscol = -1;
@@ -39,13 +43,16 @@ if ( $mode == MODE_TEAMS )
   manageTeams($client);
 if ( $mode == MODE_UNWATCH )
   unwatch($client);
+if ( $mode == MODE_ISSUES )
+  listIssues($client);
 
-echo "\nThis execution run called $apicalls Github API calls.\n";
+if ( !$quiet )
+  echo "\nThis execution run called $apicalls Github API calls.\n";
 exit;
 
 
 function checkParams() {
-  global $userorg, $token, $csvfile, $reposcol, $descscol, $argv, $argc, $public, $userscol, $teamscol, $watch, $mode, $dryrun;
+  global $userorg, $token, $csvfile, $reposcol, $descscol, $argv, $argc, $public, $userscol, $teamscol, $watch, $mode, $dryrun, $repo, $output, $quiet;
   for ( $i = 1; $i < $argc; $i++ ) {
     switch($argv[$i]) {
     case "-dryrun":
@@ -60,17 +67,29 @@ function checkParams() {
     case "-create-repos":
       $mode = MODE_CREATE;
       break;
+    case "-list-issues":
+      $mode = MODE_ISSUES;
+      break;
     case "-manage-teams":
       $mode = MODE_TEAMS;
       break;
     case "-unwatch":
       $mode = MODE_UNWATCH;
       break;
+    case "-repo":
+      $repo = $argv[++$i];
+      break;
+    case "-output":
+      $output = $argv[++$i];
+      break;
     case "-reposcol":
       $reposcol = $argv[++$i];
       if ( !is_numeric($reposcol) )
 	die ("The -reposcol value must be numeric\n");
       $reposcol--;
+      break;
+    case "-quiet":
+      $quiet = true;
       break;
     case "-descscol":
       $descscol = $argv[++$i];
@@ -115,12 +134,14 @@ function checkParams() {
     die ("Nothing to do!  You must specify either -create-repos, -manage-teams, or -unwatch\n");
   if ( ($mode != MODE_UNWATCH) && !$userorg )
     die ("You must specify the github user or organization via -org\n");
-  if ( !$csvfile )
+  if ( ($mode != MODE_ISSUES) && !$csvfile )
     die ("You must specify a CSV file to use via -csv\n");
   if ( !$token ) {
     echo "Enter the API token: ";
     $token = readline();
   }
+  if ( ($mode == MODE_ISSUES) && (!$repo) )
+    die ("You must specify the repository to use via the -repo flag\n");
 }
 
 function readCSVHeader($fp) {
@@ -198,6 +219,48 @@ function unwatch($client) {
     die ("Exception thrown: " . $e->getMessage() . "\n");
   }
   echo "\n$unwatched repos were unwatched, and $notexistant specified repos did not exist.\n";
+}
+
+// sample usage:
+// php main.php -list-issues -token `cat ../../work/slp/github/token` -org uva-slp -repo test -output issues.csv
+function listIssues($client) {
+  global $userorg, $repo, $output;
+  $paginator = new Github\ResultPager($client);
+
+  // get list of labels
+  $ghlabels = $client->api('issue')->labels()->all($userorg,$repo);
+  $labels = array();
+  foreach ( $ghlabels as $ghlabel )
+    $labels[] = str_replace("\t"," ",$ghlabel['name']);
+
+  // get list of issues
+  //$ghissues = $client->api('issue')->all($userorg,$repo);
+  $params = array($userorg,$repo,array('state'=>'all'));
+  $ghissues = $paginator->fetchAll($client->api('issue'), 'all', $params);
+  //print_r($ghissues);
+  $issues = array();
+  foreach ( $ghissues as $ghissue ) {
+    if ( isset($ghissue['pull_request']) )
+	 continue;
+    $ilabels = array();
+    foreach ( $ghissue['labels'] as $l )
+      $ilabels[] = $l['name'];
+    $issues[] = array('number'=>$ghissue['number'],
+		      'state'=>(($ghissue['closed_at']=="")?"open":"closed"),
+		      'title'=>$ghissue['title'], 
+		      'labels'=>$ilabels);
+  }
+  //print_r($issues);
+
+  // CSV output format
+  if ( $output )
+    $fp = fopen($output,"w");
+  else
+    $fp = fopen("php://stdout", "w");
+  foreach ( $issues as $issue ) {
+    fwrite ($fp, $userorg . "," . $repo . "," . $issue['number'] . "," . $issue['state'] . ",\"" . str_replace("\t"," ",$issue['title']) . "\",\"" . implode(",",$issue['labels']) . "\"\n");
+  }
+  fclose($fp);
 }
 
 function createRepos($client) {
@@ -378,8 +441,14 @@ function manageTeams($client) {
       // add members
       foreach ( $desired_members as $member )
 	if ( !in_array(strtolower($member),array_keys($actual_members)) ) {
-	  if ( !$dryrun )
-	    $client->api('organization')->teams()->addMember($ghteams[$team],$member);
+	  if ( !$dryrun ) {
+	    echo "attempting to add $member to " . $ghteams[$team] . "/$team...\n";
+	    try {
+	      $client->api('organization')->teams()->addMember($ghteams[$team],$member);
+	    } catch (Exception $e) {
+	      echo 'Caught exception: ',  $e->getMessage(), "\n";
+	    }
+	  }
           $apicalls++;
 	  echo "\tadded $member to team '$team'\n";
 	}
